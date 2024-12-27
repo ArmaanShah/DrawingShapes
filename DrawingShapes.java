c4b5e106ba18c96bd72e75e34c3870358954e571
ArmaanShah <69207811+ArmaanShah@users.noreply.github.com>
2024-12-26 21:01:22 -0500

    DrawingShapes.java

--- DrawingShapes.java
+++ DrawingShapes.java
@@ -2,29 +2,7 @@
 /*
  * DrawingShapes.java
  * Author:  Armaan Shah
- * Submission Date: October 31st 2024
- *
- *This program does the following:
- *The following code takes an input of a character from the user and 
- *follows the code to draw the corresponding shape with *. After entering the 
- *character the user wants the program will prompt the user with more information such as
- *length or height and ensure that all values are valid. 
- *
- * Statement of Academic Honesty:
- *
- * The following code represents my own work. I have neither 
- * received nor given inappropriate assistance. I have not copied 
- * or modified code from any source other than the course webpage 
- * or the course textbook. I recognize that any unauthorized 
- * assistance or plagiarism will be handled in accordance with 
- * the University of Georgia's Academic Honesty Policy and the 
- * policies of this course. I recognize that my work is based 
- * on an assignment created by the School of Computing 
- * at the University of Georgia. Any publishing or
- * posting of source code for this assignment is strictly 
- * prohibited unless you have written consent from the 
- * School of Computing at the University of Georgia.  
- */
+
 import java.util.Scanner;
 
 public class DrawingShapes {
